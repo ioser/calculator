b574a649fa4c767b371dfdd97ddd9a64a9b7dbc7
Richard Millet <milletdesigns@me.com>
2013-01-18 23:31:39 -0800

    No comment.  Added a middle initial.

--- Calculator/SCSViewController.m
+++ Calculator/SCSViewController.m
@@ -3,7 +3,7 @@
 //  Calculator
 //
 //  Created by Richard E Millet on 1/18/13.
-//  Copyright (c) 2013 Richard Millet. All rights reserved.
+//  Copyright (c) 2013 Richard E Millet. All rights reserved.
 //
 
 #import "SCSViewController.h"
